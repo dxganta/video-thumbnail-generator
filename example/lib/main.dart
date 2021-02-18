67c527b24565f8de237e8c89831ae1bb19aa47e6
realdiganta <digantakalita.ai@gmail.com>
2021-02-19 00:58:02 +0530

    Issue #1 fixed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -40,6 +40,16 @@ class HomePage extends StatelessWidget {
               "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4",
           width: 400,
           height: 400,
+          errorBuilder: (context, error, stack) {
+            return Container(
+              width: 400.0,
+              height: 400.0,
+              color: Colors.blue,
+              child: Center(
+                child: Text("error loading Image"),
+              ),
+            );
+          },
         )),
       ),
     );
--- lib/video_thumbnail_generator.dart
+++ lib/video_thumbnail_generator.dart
@@ -230,6 +230,7 @@ class ThumbnailImage extends StatelessWidget {
     return FutureBuilder<String>(
       future: getThumbnailFromVideo(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
+        print(snapshot.connectionState);
         switch (snapshot.connectionState) {
           case ConnectionState.none:
           case ConnectionState.waiting:
@@ -237,6 +238,30 @@ class ThumbnailImage extends StatelessWidget {
             break;
           case ConnectionState.active:
           case ConnectionState.done:
+            if (snapshot.data == null) {
+              return Image.network(
+                "http://no_image",
+                width: width,
+                height: height,
+                scale: scale,
+                frameBuilder: frameBuilder,
+                errorBuilder: errorBuilder,
+                semanticLabel: semanticLabel,
+                excludeFromSemantics: excludeFromSemantics,
+                color: color,
+                colorBlendMode: colorBlendMode,
+                fit: fit,
+                alignment: alignment,
+                repeat: repeat,
+                centerSlice: centerSlice,
+                matchTextDirection: matchTextDirection,
+                gaplessPlayback: gaplessPlayback,
+                isAntiAlias: isAntiAlias,
+                filterQuality: filterQuality,
+                cacheHeight: cacheHeight,
+                cacheWidth: cacheWidth,
+              );
+            }
             return Image.memory(
               base64Decode(snapshot.data),
               width: width,
